55b71f0451432df6b4e7e7153a96061a5aee6121
Redve <redvelolbanned@gmail.com>
2024-02-19 23:11:55 +0100

    Added project files

---
+++ src/datastore/error.rs
@@ -0,0 +1,33 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DatastoreError {
+    #[error("Serialization Error. Transaction rolled back.")]
+    Serialization,
+    #[error("Error setting durability offset")]
+    DurabilityOffsetError,
+    #[error("Cannot deserialize insert.")]
+    InsertDeserializationError,
+    #[error("Cannot deserialize delete.")]
+    DeleteDeserializationError,
+    #[error("Cannot make a mutable transaction if not the leader.")]
+    NotLeader,
+}
+
+impl Default for DatastoreError {
+    fn default() -> Self {
+        Self::Serialization
+    }
+}
+
+impl From<std::io::Error> for DatastoreError {
+    fn from(_err: std::io::Error) -> Self {
+        DatastoreError::InsertDeserializationError
+    }
+}
+
+impl From<std::str::Utf8Error> for DatastoreError {
+    fn from(_err: std::str::Utf8Error) -> Self {
+        DatastoreError::DeleteDeserializationError
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+pub mod datastore;
+pub mod durability;
+pub mod node;
+
+use std::collections::HashMap;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
