dc40eaf58bee8dc91246eed4c3cee41aa92177aa
Your Name <you@example.com>
2024-03-01 15:56:06 +0100

    fixing merge

--- src/durability/omnipaxos_durability.rs
+++ src/durability/omnipaxos_durability.rs
@@ -1,18 +1,14 @@
-use omnipaxos::util::NodeId;
-use std::sync::{Mutex, Arc};
+use super::log_entry::LogEntry;
 use super::*;
-use crate::durability::example_durability
-
+use crate::datastore::example_datastore::Tx;
+use crate::datastore::{tx_data::TxData, TxOffset};
+use omnipaxos::util::LogEntry as Omni_LogEntry;
+use omnipaxos::{messages::Message, util::NodeId, OmniPaxos};
+use omnipaxos_storage::memory_storage::MemoryStorage;
 /// OmniPaxosDurability is a OmniPaxos node that should provide the replicated
 /// implementation of the DurabilityLayer trait required by the Datastore.
 pub struct OmniPaxosDurability {
-    // TODO
-    id: NodeId,
-    leader: NodeId,
-    peers: Vec<NodeId>,
-    txoffset: TxOffset,
-    durable_txoffset: TxOffset,
-    data: RwLock<Vec<(TxOffset, TxData)>>,
+    pub omni_paxos: OmniPaxos<LogEntry, MemoryStorage<LogEntry>>
 }
 
 impl DurabilityLayer for OmniPaxosDurability {
@@ -24,18 +20,17 @@ impl DurabilityLayer for OmniPaxosDurability {
         &self,
         offset: TxOffset,
     ) -> Box<dyn Iterator<Item = (TxOffset, TxData)>> {
-        let data = self.data.read().unwrap();
-        let filtered_data = data.iter().filter(move |&(tx_offset, _)| *tx_offset >= offset);
-        Box::new(filtered_data.map(|&(tx_offset, ref tx_data)| (tx_offset, tx_data.clone())))
-}
-
+        todo!()
     }
 
     fn append_tx(&mut self, tx_offset: TxOffset, tx_data: TxData) {
         todo!()
     }
-
+    
+    // This is a function to decide the transaction durable offset
+    // We ask the omnipaxos to return the decided index
     fn get_durable_tx_offset(&self) -> TxOffset {
-        todo!()
+        let decided_index : u64 = self.omni_paxos.get_decided_idx();
+        TxOffset(decided_index)
     }
 }
