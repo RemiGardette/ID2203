50cfab799328aea248557b466807da44828b62a2
Your Name <you@example.com>
2024-03-01 17:40:08 +0100

    iter_starting_from_offset v1, which is just a simply modified version of iter

--- src/durability/omnipaxos_durability.rs
+++ src/durability/omnipaxos_durability.rs
@@ -62,10 +62,12 @@ pub struct OmniPaxosDurability {
 
 impl DurabilityLayer for OmniPaxosDurability {
 
+    //We read all the entries from the omnipaxos log and return an iterator over them
     fn iter(&self) -> Box<dyn Iterator<Item = (TxOffset, TxData)>> {
         if let Some(entries) = &self.omni_paxos.read_entries(..) {
             let entry_iter = entries.iter().flat_map(|entry| {
                 match entry {
+                    //We read both the decided logs and undecided logs, maybe this behaviour will need to be adjusted later
                     LogEntry::Decided(log) | LogEntry::Undecided(log) => {
                         Some((log.tx_offset.clone(), log.tx_data.clone()))
                     }
@@ -78,11 +80,29 @@ impl DurabilityLayer for OmniPaxosDurability {
         }
     }
 
+    //Same logic as iter, but we filter out the entries with an offset stricly lower than the one provided
     fn iter_starting_from_offset(
         &self,
         offset: TxOffset,
     ) -> Box<dyn Iterator<Item = (TxOffset, TxData)>> {
-        todo!()
+        if let Some(entries) = &self.omni_paxos.read_entries(..) {
+            let entry_iter = entries.iter().filter_map(|entry| {
+                match entry {
+                    //We read both the decided logs and undecided logs, maybe this behaviour will need to be adjusted later
+                    LogEntry::Decided(log) | LogEntry::Undecided(log) => {
+                        if log.tx_offset >= offset {
+                            Some((log.tx_offset.clone(), log.tx_data.clone()))
+                        } else {
+                            None
+                        }
+                    }
+                    _ => None,
+                }
+            });
+            Box::new(entry_iter.collect::<Vec<_>>().into_iter())
+        } else {
+            Box::new(std::iter::empty())
+        }
     }
 
     fn append_tx(&mut self, tx_offset: TxOffset, tx_data: TxData) {
