8405c488f61f2066707f64646e42b6c11f8d4a76
Your Name <you@example.com>
2024-03-03 18:12:50 +0100

    fixed the entry issue

--- src/durability/omnipaxos_durability.rs
+++ src/durability/omnipaxos_durability.rs
@@ -3,61 +3,25 @@ use self::example_durability::ExampleDurabilityLayer;
 use super::*;
 use crate::datastore::example_datastore::Tx;
 use crate::datastore::{tx_data::TxData, TxOffset};
-use omnipaxos::util::LogEntry as Omni_LogEntry;
+use omnipaxos::util::LogEntry as OmniLogEntry;
 use omnipaxos::{messages::Message, util::NodeId, OmniPaxos};
 use omnipaxos_storage::memory_storage::MemoryStorage;
+use omnipaxos::macros::Entry;
 use omnipaxos::storage::Entry;
 use omnipaxos_storage::persistent_storage::PersistentStorage;
 use omnipaxos::storage::Snapshot;
 use omnipaxos::storage::NoSnapshot;
-use omnipaxos::util::LogEntry;
 
-#[derive(Debug, Clone)]
-struct Log{
+#[derive(Debug, Clone, Entry)]
+struct LogEntry{
     tx_offset: TxOffset,
     tx_data: TxData,
-}
-
-// Implement the Entry trait for LogEntry
-impl Entry for Log{
-    #[cfg(not(feature = "serde"))]
-    /// The snapshot type for this entry type.
-    type Snapshot = NoSnapshot;
-
-    #[cfg(feature = "serde")]
-    /// The snapshot type for this entry type.
-    type Snapshot: Snapshot<Self> + Serialize + for<'a> Deserialize<'a>;
-
-    #[cfg(feature = "unicache")]
-    /// The encoded type of some data. If there is a cache hit in UniCache, the data will be replaced and get sent over the network as this type instead. E.g., if `u8` then the cached `Entry` (or field of it) will be sent as `u8` instead.
-    type Encoded: Encoded;
-    #[cfg(feature = "unicache")]
-    /// The type representing the encodable parts of an `Entry`. It can be set to `Self` if the whole `Entry` is cachable. See docs of `pre_process()` for an example of deriving `Encodable` from an `Entry`.
-    type Encodable: Encodable;
-    #[cfg(feature = "unicache")]
-    /// The type representing the **NOT** encodable parts of an `Entry`. Any `NotEncodable` data will be transmitted in its original form, without encoding. It can be set to `()` if the whole `Entry` is cachable. See docs of `pre_process()` for an example.
-    type NotEncodable: NotEncodable;
-
-    #[cfg(all(feature = "unicache", not(feature = "serde")))]
-    /// The type that represents if there was a cache hit or miss in UniCache.
-    type EncodeResult: Clone + Debug;
-
-    #[cfg(all(feature = "unicache", feature = "serde"))]
-    /// The type that represents the results of trying to encode i.e., if there was a cache hit or miss in UniCache.
-    type EncodeResult: Clone + Debug + Serialize + for<'a> Deserialize<'a>;
-
-    #[cfg(all(feature = "unicache", not(feature = "serde")))]
-    /// The type that represents the results of trying to encode i.e., if there was a cache hit or miss in UniCache.
-    type UniCache: UniCache<T = Self>;
-    #[cfg(all(feature = "unicache", feature = "serde"))]
-    /// The unicache type for caching popular/re-occurring fields of an entry.
-    type UniCache: UniCache<T = Self> + Serialize + for<'a> Deserialize<'a>;
 }
 
 /// OmniPaxosDurability is a OmniPaxos node that should provide the replicated
 /// implementation of the DurabilityLayer trait required by the Datastore.
 pub struct OmniPaxosDurability {
-    omni_paxos: OmniPaxos<Log, MemoryStorage<Log>>
+    omni_paxos: OmniPaxos<LogEntry, MemoryStorage<LogEntry>>
 }
 
 impl DurabilityLayer for OmniPaxosDurability {
@@ -68,7 +32,7 @@ impl DurabilityLayer for OmniPaxosDurability {
             let entry_iter = entries.iter().flat_map(|entry| {
                 match entry {
                     //We read both the decided logs and undecided logs, maybe this behaviour will need to be adjusted later
-                    LogEntry::Decided(log) | LogEntry::Undecided(log) => {
+                    OmniLogEntry::Decided(log) | OmniLogEntry::Undecided(log) => {
                         Some((log.tx_offset.clone(), log.tx_data.clone()))
                     }
                     _ => None,
@@ -89,7 +53,7 @@ impl DurabilityLayer for OmniPaxosDurability {
             let entry_iter = entries.iter().filter_map(|entry| {
                 match entry {
                     //We read both the decided logs and undecided logs, maybe this behaviour will need to be adjusted later
-                    LogEntry::Decided(log) | LogEntry::Undecided(log) => {
+                    OmniLogEntry::Decided(log) | OmniLogEntry::Undecided(log) => {
                         if log.tx_offset >= offset {
                             Some((log.tx_offset.clone(), log.tx_data.clone()))
                         } else {
@@ -106,7 +70,11 @@ impl DurabilityLayer for OmniPaxosDurability {
     }
 
     fn append_tx(&mut self, tx_offset: TxOffset, tx_data: TxData) {
-        todo!()
+        let log = LogEntry {
+            tx_offset,
+            tx_data,
+        };
+        let _ = self.omni_paxos.append(log);
     }
     
     // This is a function to decide the transaction durable offset
